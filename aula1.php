226f164d2e35dc3ee131dae5dd980ba44f7de82d
Brian Neukamp <82863920006@senacrs.edu.br>
2024-06-17 20:15:20 -0300

    Aulas PHP

---
+++ aula1.php
@@ -0,0 +1,11 @@
+<?php
+    //echo "Aula1";
+    $nome   = "Felipe";
+    $idade  = 20;
+    $altura = 1.70;
+    $logico = true;
+    const senac = "Caxias";
+
+    echo $nome . " " . $idade . " " . $altura . " " . $logico;
+    echo senac;
+?>
